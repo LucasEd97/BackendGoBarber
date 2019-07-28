0595dff7a182305b9245a1cdd2b51b8e0169d132
Lucas Ferreira <lucas.98713@gmail.com>
2019-07-28 17:44:09 -0300

    Configura Sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ messa: "Hello Dude" });
+routes.get('/', (req, res) => {
+  return res.json({ messa: 'Hello Dude' });
 });
 
 export default routes;
